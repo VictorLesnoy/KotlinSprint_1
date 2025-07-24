0b24d7b789ee2168d4b3527329d9854b29bc2812
VictorLesnoy <martynovv@list.ru>
2025-07-24 22:43:15 +0500

    KS-2-5 Percent

--- src/main/kotlin/Lesson_2/Lesson2_Task5.kt
+++ src/main/kotlin/Lesson_2/Lesson2_Task5.kt
@@ -1,9 +1,12 @@
 package org.example.Lesson_2
 
+
 fun main() {
         val deposit = 70000
         val percent = 16.7
         val depositTerm = 20.0
+        val const1 = 1
+        val const2 = 100.0
 
-        println("Сумма через 20 лет :%.3f".format((deposit * (Math.pow((1 + (percent / 100.0)),depositTerm)))))
+        println("Сумма через 20 лет :%.3f".format((deposit * (const1 + (percent / const2)).pow(depositTerm))))
 }
