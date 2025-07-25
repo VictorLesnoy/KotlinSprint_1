2b945dfe52ae433072da7a2f0846ee5362eec772
VictorLesnoy <martynovv@list.ru>
2025-07-25 20:21:57 +0500

    KS-2-5

---
+++ src/main/kotlin/Lesson_3/KS-3-1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson_3
+
+fun main() {
+    var greeting = "Good afternoon"
+    val userName = "Pentakl"
+    println("$greeting, $userName")
+    greeting = "Good evening"
+    println("$greeting, $userName")
+}
