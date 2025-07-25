e4b931cbdf0b49f4365f37dfd63c480724fd5581
VictorLesnoy <martynovv@list.ru>
2025-07-25 20:04:45 +0500

    KS-2-5

--- src/main/kotlin/Lesson_2/Lesson2_Task5.kt
+++ src/main/kotlin/Lesson_2/Lesson2_Task5.kt
@@ -1,12 +1,13 @@
 package org.example.Lesson_2
 
+import kotlin.math.pow
 
 fun main() {
         val deposit = 70000
         val percent = 16.7
         val depositTerm = 20.0
-        val const1 = 1
-        val const2 = 100.0
+        val baseRate = 1
+        val hundred = 100.0
 
-        println("Сумма через 20 лет :%.3f".format((deposit * (const1 + (percent / const2)).pow(depositTerm))))
+        println("Сумма через 20 лет :%.3f".format((deposit * (baseRate + (percent / hundred)).pow(depositTerm))))
 }
