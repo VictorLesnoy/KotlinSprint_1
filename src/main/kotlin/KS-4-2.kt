a04c212aed1e4b62332d17a860add7603508231e
VictorLesnoy <martynovv@list.ru>
2025-07-28 22:46:11 +0500

    KS-4-2

--- src/main/kotlin/KS-4-2.kt
+++ src/main/kotlin/KS-4-2.kt
@@ -14,6 +14,7 @@ fun main() {
     println("Груз весом $weight кг и объемом $value л соответствует категории 'Average':" +
             " ${(weight > WEIGHT_MIN) && (weight <= WEIGHT_MAX) && (value < VALUE_MAX)}")
 }
+
 const val WEIGHT_MIN = 35
 const val WEIGHT_MAX = 100
 const val VALUE_MAX = 100
