13ffc75adc0f544774430b04bee388bff0ff9ddf
VictorLesnoy <martynovv@list.ru>
2025-07-29 23:40:46 +0500

    KS-4-4

---
+++ src/main/kotlin/KS-4-4.kt
@@ -0,0 +1,10 @@
+package org.example
+
+fun main() {
+    val day = 5
+    if (day % 2 == 0) {println("Упражнения для рук: false \nУпражнения для ног: true \nУпражнения для спины: true \nУпражнения для пресса: false")
+    } else {
+        println("Упражнения для рук: true \nУпражнения для ног: false \nУпражнения для спины: false \nУпражнения для пресса: true")
+    }
+
+}
