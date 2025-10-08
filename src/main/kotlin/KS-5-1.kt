e2d9c3ef7a7bef2bee0191bd61c779e1bd259240
VictorLesnoy <martynovv@list.ru>
2025-10-08 22:20:58 +0500

    KS-5-1

--- src/main/kotlin/KS-5-1.kt
+++ src/main/kotlin/KS-5-1.kt
@@ -7,6 +7,7 @@ fun main() {
     println("Докажите, что вы не бот - решите пример: " + randomNumberOne + "+" + randomNumberTwo + "=?")
     val answer = readln().toInt()
     val ANSWER = randomNumberOne + randomNumberTwo
+
         if (answer == ANSWER) {
         println("Добро пожаловать!")
     } else {
