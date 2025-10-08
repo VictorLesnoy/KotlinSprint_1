aaf523b93c83c74fe718811409eaf9e80a533773
VictorLesnoy <martynovv@list.ru>
2025-10-08 22:18:38 +0500

    KS-5-1

--- src/main/kotlin/KS-5-1.kt
+++ src/main/kotlin/KS-5-1.kt
@@ -1,12 +1,15 @@
 package org.example
 
 fun main() {
-    println("Докажите, что вы не бот - решите пример: 7+2=?")
+    val randomRange = 0..9
+    val randomNumberOne = randomRange.random()
+    val randomNumberTwo = randomRange.random()
+    println("Докажите, что вы не бот - решите пример: " + randomNumberOne + "+" + randomNumberTwo + "=?")
     val answer = readln().toInt()
+    val ANSWER = randomNumberOne + randomNumberTwo
 
-    if (answer == ANSWER) {
+        if (answer == ANSWER) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")}
 }
-const val ANSWER = 9
