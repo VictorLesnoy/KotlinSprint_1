19fc14aa340fad6abc4c20ba7e3aa6f748fbe2fe
VictorLesnoy <martynovv@list.ru>
2025-10-09 18:18:03 +0500

    KS-5-1 rebild

--- src/main/kotlin/KS-5-1.kt
+++ src/main/kotlin/KS-5-1.kt
@@ -4,12 +4,12 @@ fun main() {
     val randomRange = 0..9
     val randomNumberOne = randomRange.random()
     val randomNumberTwo = randomRange.random()
-    println("Докажите, что вы не бот - решите пример: " + randomNumberOne + "+" + randomNumberTwo + "=?")
+    println("Докажите, что вы не бот - решите пример: $randomNumberOne + $randomNumberTwo =?")
     val answer = readln().toInt()
-    val ANSWER = randomNumberOne + randomNumberTwo
 
-        if (answer == ANSWER) {
+        if (answer == randomNumberOne + randomNumberTwo) {
         println("Добро пожаловать!")
     } else {
-        println("Доступ запрещен.")}
+        println("Доступ запрещен.")
+    }
 }
