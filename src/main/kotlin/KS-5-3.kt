214c9303c335b1fe57abb7e25599b3508c5c15e0
VictorLesnoy <martynovv@list.ru>
2025-10-11 10:08:28 +0500

    KS-5-3 rebild

--- src/main/kotlin/KS-5-3.kt
+++ src/main/kotlin/KS-5-3.kt
@@ -1,16 +1,18 @@
 package org.example
 
 fun main() {
-    val numberOne = 15
-    val numberTwo = 20
+    val randomRange = 0..42
+    val numberOne = randomRange.random()
+    val numberTwo = randomRange.random()
     println("Введите первое числоот 0 до 42")
     val answerOne = readln().toInt()
     println("ВВедите второе число от 0 до 42")
     val answerTwo = readln().toInt()
+
     if (answerOne == numberOne && answerTwo == numberTwo) {
         println("Поздравляем! Вы выиграли главный приз!")
     } else if (answerOne == numberOne || answerTwo == numberTwo) {
-            println("Вы выиграли утешительный приз!")
+        println("Вы выиграли утешительный приз!")
     } else {
         println("Неудача!")
     }
