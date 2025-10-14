20721131fab292aa6438587c982ea76867de03bb
VictorLesnoy <martynovv@list.ru>
2025-10-14 13:39:22 +0500

    KS-5-5 rebild

--- src/main/kotlin/KS-5-5.kt
+++ src/main/kotlin/KS-5-5.kt
@@ -1,11 +1,11 @@
 package org.example
 
 fun main() {
-    val randomRange = (0..42)
+    val randomRange = 0..42
     val randomNumber = List(3) { randomRange.random() }
     println("Введите через пробел три числа от 0 до 42")
     val answer = readln().split(' ').map { it.toInt() }
-    val concidences = (answer.intersect(randomNumber)).size
+    val concidences = answer.intersect(randomNumber).size
 
     when (concidences) {
         0 -> println("Неудача! Вы не угадали ни одного числа.")
