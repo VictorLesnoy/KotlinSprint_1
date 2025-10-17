b145c11c5367850bc6b833c229748b2f8a34a213
VictorLesnoy <martynovv@list.ru>
2025-10-17 09:23:44 +0500

    KS-7-1 rebild

--- src/main/kotlin/KS-7-1.kt
+++ src/main/kotlin/KS-7-1.kt
@@ -1,9 +1,19 @@
 package org.example
 
 fun main() {
-    val password = ('a'..'z')+ ('0'..'9')
+    val letters = ('a'..'z')
+    val numbers = ('0'..'9')
+
+    val password = StringBuilder()
+
     print("Ваш пароль: ")
-    for (i in 1..6) {
-        print(password.random())
+    for (i in 0 until 6) {
+        if (i % 2 == 0) {
+            password.append(letters.random())
+        } else {
+            password.append(numbers.random())
+        }
     }
+
+    print(password)
 }
