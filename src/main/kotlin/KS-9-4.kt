fe352b7217ae0ed087a705e823ae94639c927538
VictorLesnoy <martynovv@list.ru>
2025-10-22 19:49:15 +0500

    KS-9-4 rebild

--- src/main/kotlin/KS-9-4.kt
+++ src/main/kotlin/KS-9-4.kt
@@ -2,8 +2,8 @@ package org.example
 
 fun main() {
     println("Введите 5 ингридиентов блюда, через запятую с пробелом.")
-    val input = readLine()
-    val  ingridients = input?.split(", ")
-    val newIngridients = ingridients?.sorted()
+    val input = readln()
+    val ingridients = input.split(", ")
+    val newIngridients = ingridients.sorted()
     println("Новый порядок ингридиентов:\n$newIngridients")
 }
