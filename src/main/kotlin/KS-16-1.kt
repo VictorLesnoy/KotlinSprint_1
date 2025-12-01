dbb5a62e72c90505c00739d820d27d05b0801704
VictorLesnoy <martynovv@list.ru>
2025-12-01 18:51:36 +0500

    KS-16-1

--- src/main/kotlin/KS-16-1.kt
+++ src/main/kotlin/KS-16-1.kt
@@ -3,12 +3,10 @@ package org.example
 import kotlin.random.Random
 
 class Cube {
-
     fun roll() {
         val randomNumber = Random().nextInt(1,7)
         println("На кубике выпало $randomNumber")
     }
-
 }
 
 fun main() {
