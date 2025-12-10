272cd0c8972ff59e6a0f34ea0583a601231659b1
VictorLesnoy <martynovv@list.ru>
2025-12-10 21:16:18 +0500

    KS-17-2rebild

--- src/main/kotlin/KS-17-2.kt
+++ src/main/kotlin/KS-17-2.kt
@@ -1,22 +1,20 @@
 package org.example
 
-class Ship(name: String, averageSpeed: Double, homeport: String) {
+class Ship(name: String, var averageSpeed: Double, var homeport: String) {
     var name = name
         get() = field
-        set(value: String) {
-            if (value != "Fastest") {
-                println("Имя корабля менять запрещено!")
-            } else {
+        set(value) {
+            if (value == "Fastest") {
                 field = value
+            } else {
+                println("Имя корабля менять запрещено!")
             }
         }
-    var averageSpeed = 0.0
-    var homeport = "Pakistan"
 }
 
 fun main() {
     var ship = Ship("Fastest", 25.2, "London")
     println("Корабль ${ship.name}, средняя скорость ${ship.averageSpeed}, порт приписки ${ship.homeport}  ")
-    ship.name = "hsdg"
-    println("Попытка изменить имя: ${ship.name}")
+    ship.name = "Fast"
+    println("Попытка изменить имя на: ${ship.name}")
 }
