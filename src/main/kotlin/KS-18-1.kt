f25cc1472419b210dc1d75740f01e376a56a210f
VictorLesnoy <martynovv@list.ru>
2025-12-22 22:12:09 +0500

    KS-18-1

---
+++ src/main/kotlin/KS-18-1.kt
@@ -0,0 +1,33 @@
+package org.example
+
+class Items {
+
+    fun addItem(
+        orderNumber: Long,
+        type: String,
+    ) {
+        println("Заказан товар: $type.")
+    }
+
+    fun addItem(
+        orderNumber: Long,
+        type: List<String>,
+    ) {
+        println("Заказаны товары: $type.")
+    }
+
+}
+
+fun main() {
+    val items = Items()
+
+    items.addItem(
+        1,
+        "Хлеб",
+    )
+
+    items.addItem(
+        2,
+        listOf("болт", "шуруп", "гвоздь")
+    )
+}
