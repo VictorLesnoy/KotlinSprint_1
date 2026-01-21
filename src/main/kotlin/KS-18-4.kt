93010b7806caa6ca57408c2ca60e7a6d64bfc1ae
VictorLesnoy <martynovv@list.ru>
2026-01-21 18:26:41 +0500

    KS-18-4final

--- src/main/kotlin/KS-18-4.kt
+++ src/main/kotlin/KS-18-4.kt
@@ -10,23 +10,24 @@ abstract class Parcel(
 
 class Rectangular(
     name: String,
+    val width: Double,
     val height: Double,
     val length: Double,
 ) : Parcel(name) {
-    override fun area() = height * length
+    override fun area() = 2 * ((height * length) + (height * width) + (width * length))
 }
 
 class Cube(
     name: String,
     val side: Double,
 ) : Parcel(name) {
-    override fun area() = side.pow(2)
+    override fun area() = 6 * side.pow(2)
 }
 
 fun main() {
-    val parcel1: Parcel = Rectangular("коробка 1",20.0, 30.0)
+    val parcel1: Parcel = Rectangular("коробка 1",20.0, 30.0, 10.0)
     val parcel2: Parcel = Cube("коробка 2", 5.0)
 
-    println("Площадь отправления '${parcel1.name}' равна ${parcel1.area()}")
-    println("Площадь отправления '${parcel2.name}' равна ${parcel2.area()}")
+    println("Площадь поверхности отправления '${parcel1.name}' равна ${parcel1.area()}")
+    println("Площадь поверхности отправления '${parcel2.name}' равна ${parcel2.area()}")
 }
