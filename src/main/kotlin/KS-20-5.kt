6470a95d2caebfbae932d8f57237e3b97c500cec
VictorLesnoy <martynovv@list.ru>
2026-01-31 19:56:59 +0500

    KS-20-5rebild

--- src/main/kotlin/KS-20-5.kt
+++ src/main/kotlin/KS-20-5.kt
@@ -2,6 +2,7 @@ package org.example
 
 class Robot {
     private var modifier: (String) -> String = {it}
+    val phrases = listOf("Как твои дела ?", "Сидим весело курим", "Правильное здоровое питание", "Маленький летящий самолет", "Подумаем об этом потом")
 
     fun setModifier(modifier: (String) -> String) {
         this.modifier = modifier
@@ -13,9 +14,8 @@ class Robot {
 }
 
 fun main() {
-    val phrases = listOf("Как твои дела ?", "Сидим весело курим", "Правильное здоровое питание", "Маленький летящий самолет", "Подумаем об этом потом")
-    val phrase = phrases.random()
     val robot = Robot()
+    val phrase = robot.phrases.random()
 
     robot.setModifier { message ->
         message
